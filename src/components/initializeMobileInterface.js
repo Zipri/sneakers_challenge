134c4072403e04092af45646cda70a195db3bf59
KirillS <kirill.serov.00@mail.ru>
2023-01-13 16:02:11 +0300

    0.5.1. InitializeMobileInterface: added slider logic

--- src/components/initializeMobileInterface.js
+++ src/components/initializeMobileInterface.js
@@ -1,4 +1,4 @@
-export default function initializeMobileInterface () {
+export default function initializeMobileInterface() {
   if (window.innerWidth <= 768) {
     createHeaderMenu();
     createSliderArrows();
@@ -17,11 +17,44 @@ export default function initializeMobileInterface () {
 }
 
 function createHeaderMenu() {
+  let isOpen = false;
   const header = document.querySelector('header[data-header]');
   let menu = document.createElement('div');
   menu.className = 'header__menu';
   menu.innerHTML = '<img src="assets/images/icon-menu.svg" alt="menu">'
-  header.prepend(menu);
+  header.firstElementChild.prepend(menu);
+  menu.addEventListener('click', function (event) {
+    if (isOpen) {
+      const menuList = document.querySelector('.header__menu_list');
+      menuList.parentNode.removeChild(menuList)
+      isOpen = false;
+    } else {
+      let menuList = document.createElement('ul');
+      menuList.classList.add('header__menu_list');
+      menuList.classList.add('flex');
+      menuList.classList.add('flex__center_space_between');
+      menuList.classList.add('flex__gap_small');
+      menuList.classList.add('flex__wrap');
+      menuList.innerHTML = `
+      <li class="header__menu_list_item">
+        <a target="_blank" href="/">Collections</a>
+      </li>
+      <li class="header__menu_list_item">
+        <a target="_blank" href="/">Men</a>
+      </li>
+      <li class="header__menu_list_item">
+        <a target="_blank" href="/">Women</a>
+      </li>
+      <li class="header__menu_list_item">
+        <a target="_blank" href="/">About</a>
+      </li>
+      <li class="header__menu_list_item">
+        <a target="_blank" href="/">Contact</a>
+      </li>`;
+      header.append(menuList);
+      isOpen = true;
+    }
+  })
 }
 
 function deleteHeaderMenu() {
@@ -34,9 +67,27 @@ function createSliderArrows() {
   let arrows = document.createElement('div');
   arrows.className = 'slider__arrows';
   arrows.innerHTML = `
-    <button class="slider__arrows__arrow">&#9668;</button>
-    <button class="slider__arrows__arrow">&#9658;</button>`;
+    <button data-arrow-prev class="slider__arrows__arrow">
+      <img src="assets/images/icon-previous.svg" alt="prev">
+    </button>
+    <button data-arrow-next class="slider__arrows__arrow">
+      <img src="assets/images/icon-next.svg" alt="next">
+    </button>`;
   slider.prepend(arrows);
+  const image = slider.querySelector('.slider__photo_large');
+  let imageNumber = +image.src.at(-5);
+  arrows.querySelector('button[data-arrow-prev]').addEventListener('click', () => {
+    if (imageNumber > 1) {
+      imageNumber -= 1;
+      image.src = `assets/images/image-product-${imageNumber}.jpg`;
+    }
+  });
+  arrows.querySelector('button[data-arrow-next]').addEventListener('click', () => {
+    if (imageNumber < 4) {
+      imageNumber += 1;
+      image.src = `assets/images/image-product-${imageNumber}.jpg`;
+    }
+  });
 }
 
 function deleteSliderArrows() {
