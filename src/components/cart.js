4bbb526208b3a7bbb6a3817fc29bcd212d05782b
Zipri <kirill.serov.00@mail.ru>
2023-01-14 18:13:31 +0300

    0.7.1. CartWindow: user can add new item

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,14 +1,19 @@
-const openUserCart = (state, cart) => {
-  let isOpen = false;
-  cart.addEventListener('click', function (event) {
-    const cartWindow = document.createElement('div');
-    if (isOpen) {
-      const cartModule = document.querySelector('.cart__window');
-      cartModule.parentNode.removeChild(cartModule);
-      isOpen = false;
-    } else {
-      cartWindow.className = 'cart__window';
-      cartWindow.innerHTML = `
+const emptyCart = (cartWindow, state) => {
+  cartWindow.innerHTML = `
+      <h2 class="title_medium cart__title">Cart</h2>
+      <hr class="divider">
+      <div class="cart__item cart__padding flex flex__center_space_between">
+        <div class="title_medium">Your cart is empty</div>
+      </div>`;
+  state.cartItems = 0;
+}
+
+const createUserCart = (state) => {
+  const price = state.cartItems * 125;
+  const cartWindow = document.createElement('div');
+  cartWindow.className = 'cart__window';
+  if (state.cartItems) {
+    cartWindow.innerHTML = `
       <h2 class="title_medium cart__title">Cart</h2>
       <hr class="divider">
       <div class="cart__item cart__padding flex flex__center_space_between">
@@ -20,11 +25,11 @@ const openUserCart = (state, cart) => {
           </div>
           <div class="flex flex__center_space_between flex__gap_small">
             <div class="cart__text_info">
-              $125.00 x 1 <b>$125.00</b>
+              $125.00 x ${state.cartItems} <b>$${price}.00</b>
             </div>
           </div>
         </div>
-        <button>
+        <button data-delete-from-cart class="cart__delete_button">
           <img src="assets/images/icon-delete.svg" alt="image-product">
         </button>
       </div>
@@ -33,13 +38,47 @@ const openUserCart = (state, cart) => {
           Checkout
         </button>
       </div>`;
-      document.querySelector('body').append(cartWindow);
+    const deleteButton = cartWindow.querySelector('button[data-delete-from-cart]');
+    const cartItem = cartWindow.querySelector('.cart__item');
+    deleteButton.addEventListener('click', function (event) {
+      cartItem.parentNode.removeChild(cartItem);
+      emptyCart(cartWindow, state);
+    })
+  } else {
+    emptyCart(cartWindow, state);
+  }
+  document.querySelector('body').append(cartWindow);
+}
+
+const deleteUserCart = () => {
+  const cartModule = document.querySelector('.cart__window');
+  cartModule.parentNode.removeChild(cartModule);
+}
+
+const openUserCart = (state, cart) => {
+  let isOpen = false;
+  cart.addEventListener('click', function (event) {
+    if (isOpen) {
+      deleteUserCart();
+      isOpen = false;
+    } else {
+      createUserCart(state);
       isOpen = true;
     }
   })
 }
 
-export default function cart (state) {
+const addToCart = (state, addToCartButton) => {
+  addToCartButton.addEventListener('click', function (event) {
+    if (state.counter > 0) {
+      state.cartItems += state.counter;
+    }
+  })
+}
+
+export default function cart(state) {
   const cart = document.querySelector('button[data-cart]');
+  const addToCartButton = document.querySelector('button[data-add-to-cart]');
   openUserCart(state, cart);
+  addToCart(state, addToCartButton);
 }
--- src/state.js
+++ src/state.js
@@ -1,6 +1,7 @@
 const state = {
   counter: 0,
   sliderMainPhoto: 1,
+  cartItems: 0,
 }
 
 export default state;
